26da64828dbf0340f6fdb55fdc7184a9dbbfdabb
Flora-King <giflona@yahoo.co.uk>
2023-09-18 15:41:23 +0000

    Add code to allow user to edit/delete their reviews

--- kosi_app/urls.py
+++ kosi_app/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.CourseList.as_view(), name='home'),
-    path('', views.CourseList.as_view(), name='course_list'),
-    path('<slug:slug>/', views.CourseDetail.as_view(), name='course_detail'),
-    path('star/<slug:slug>', views.CourseStar.as_view(), name='course_star'),
-
+    path('<slug:slug>/', views.course_detail, name='course_detail'),
+    # path('star/<slug:slug>', views.course_star, name='course_star'),
+    path('<slug:slug>/delete_review/<int:review_id>', views.review_delete, name='review_delete'),
+    path('<slug:slug>/edit_review/<int:review_id>', views.review_edit, name='review_edit')
 ]
--- kosi_app/views.py
+++ kosi_app/views.py
@@ -1,97 +1,122 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic, View
+from django.views import generic
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import Course
 from .forms import ReviewForm
-from django.views.generic import ListView, DetailView
-from django.db.models import Q
-
-
-# class CourseSearch(ListView):
-#     model = Course
-#     queryset = Course.objects.all()
-
-#     def get_queryset(self, slug, *args, **kwargs):
-#         q = request.GET.get('q')
-
-#         if q:
-#             courses = Course.objects.filter(title__search=q)
-
-#         else:
-#             courses = None
-
-#         context = {'courses': courses}
-#         return render(request, 'base.html', context)
 
 
 class CourseList(generic.ListView):
+    """
+    This is Django's generic ListView. We are a little limited as to
+    what we can do with this, but if we really needed to, we could
+    access the request object here too.
+    """
     model = Course
-    queryset = Course.objects.filter(status=1).order_by('-delivery_from')
-    template_name = 'index.html'
+    queryset = Course.objects.filter(status=1).order_by("-delivery_from")
+    template_name = "course_detail.html"
     paginate_by = 9
 
 
-class CourseDetail(View):
+def course_detail(request, slug, *args, **kwargs):
+    """
+    A function-based view to view the detail of a course.
+    Largely the same as the class-based, but we don't have
+    different methods for GET and POST. Because it's not a
+    class, all of the extra "self" stuff is removed too.
 
-    def get(self, request, slug, *args, **kwargs):
-        queryset = Course.objects.filter(status=1)
-        course = get_object_or_404(queryset, slug=slug)
-        reviews = course.reviews.filter(approved=True).order_by("created_on")
-        stars = False
-        if course.stars.filter(id=self.request.user.id).exists():
-            stars = True
+    Functionally, it's the same, but it is a bit clearer
+    what's going on. To differentiate between request methods,
+    we use request.method == "GET" or request.method == "POST"
+    """
 
-        return render(
-            request,
-            "course_detail.html",
-            {
-                "course": course,
-                "reviews": reviews,
-                "commented": False,
-                "stars": stars,
-                "review_form": ReviewForm()
-            },
-        )
+    queryset = Course.objects.filter(status=1)
+    course = get_object_or_404(queryset, slug=slug)
+    reviews = course.reviews.all().order_by("-created_on")
+    review_count = course.reviews.filter(approved=True).count()
+    stars = False
+    reviewed = False
 
-    def post(self, request, slug, *args, **kwargs):
-        queryset = Course.objects.filter(status=1)
-        course = get_object_or_404(queryset, slug=slug)
-        reviews = course.reviews.filter(approved=True).order_by("created_on")
-        stars = False
-        if course.stars.filter(id=self.request.user.id).exists():
-            stars = True
+    if course.stars.filter(id=request.user.id).exists():
+        stars = True
 
+    if request.method == "POST":
         review_form = ReviewForm(data=request.POST)
-
         if review_form.is_valid():
             review_form.instance.email = request.user.email
             review_form.instance.name = request.user.username
             review = review_form.save(commit=False)
             review.course = course
             review.save()
+            messages.add_message(request, messages.SUCCESS, 'Review awaiting check.')
         else:
             review_form = ReviewForm()
+    else:
+        review_form = ReviewForm()
 
-        return render(
-            request,
-            "course_detail.html",
-            {
-                "course": course,
-                "reviews": reviews,
-                "reviewed": True,
-                "stars": stars,
-                "review_form": ReviewForm()
-            },
-        )
+    return render(
+        request,
+        "course_detail.html",
+        {
+            "course": course,
+            "reviews": reviews,
+            "review_count": review_count,
+            "stars": stars,
+            "review_form": review_form
+        },
+    )
 
 
-class CourseStar(View):
-    def post(self, request, slug):
-        course = get_object_or_404(Course, slug=slug)
+def post_like(request, slug, *args, **kwargs):
+    """
+    This view enables user to update the stars.
+    """
+    course = get_object_or_404(Course, slug=slug)
 
+    if request.method == "POST" and request.user.is_authenticated:
         if course.stars.filter(id=request.user.id).exists():
             course.stars.remove(request.user)
         else:
             course.stars.add(request.user)
 
-        return HttpResponseRedirect(reverse('course_detail', args=[slug]))
+    return HttpResponseRedirect(reverse('course_detail', args=[slug]))
+
+
+def review_delete(request, slug, review_id, *args, **kwargs):
+    """
+    This view enables user to delete reviews
+    """
+    queryset = Course.objects.filter(status=1)
+    course = get_object_or_404(queryset)
+    review = course.reviews.filter(id=review_id).first()
+
+    if review.name == request.user.username:
+        review.delete()
+        messages.add_message(request, messages.SUCCESS, 'Review deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own reviews!')
+
+    return HttpResponseRedirect(reverse('course_detail', args=[slug]))
+
+
+def review_edit(request, slug, review_id, *args, **kwargs):
+    """
+    view to edit reviews
+    """
+    if request.method == "POST":
+
+        queryset = Course.objects.filter(status=1)
+        course = get_object_or_404(queryset, slug=slug)
+        review = course.creviews.filter(id=creview_id).first()
+
+        review_form = ReviewForm(data=request.POST, instance=review)
+        if review_form.is_valid() and review.name == request.user.username:
+            review = review_form.save(commit=False)
+            review.course = course
+            review.approved = False
+            review.save()
+            messages.add_message(request, messages.SUCCESS, 'Review Successfully Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating review!')
+
+    return HttpResponseRedirect(reverse('course_detail', args=[slug]))
